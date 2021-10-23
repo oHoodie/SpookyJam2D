c367cc544f003dc3052d5661a397ce5f7546503d
Philipp Gröbelbauer <philipp.groebelbauer@math.uzh.ch>
2021-10-23 20:03:05 +0200

    basis for game over

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public SpriteRenderer blackScreen;
+    public float secondsToBlackScreen;
+
+    private bool isGameOver = false;
+    private float gameOverCounter;
+    private float secondsSinceSceneStart = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        secondsSinceSceneStart += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGameOver = true;
+        }
+
+        HandleBlackScreeen();
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+    }
+
+    private void HandleBlackScreeen()
+    {
+        if (isGameOver)
+        {
+            gameOverCounter += Time.deltaTime;
+            if (true)
+            {
+                blackScreen.color = new Color(0, 0, 0, gameOverCounter / secondsToBlackScreen);
+            }
+            if (gameOverCounter / secondsToBlackScreen >= 1)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+        else
+        {
+            float alpha = secondsSinceSceneStart >= secondsToBlackScreen ? 0 : 1 - (secondsSinceSceneStart / secondsToBlackScreen);
+            blackScreen.color = new Color(0, 0, 0, alpha);
+        }
+    }
+}
