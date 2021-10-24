1ce6c87ab1282a45073e874f58d598cc382bb2a4
Philipp Gröbelbauer <philipp.groebelbauer@math.uzh.ch>
2021-10-24 17:51:41 +0200

    shadow fix, take pic with leftclick, fix menu sound

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -26,7 +26,7 @@ public class FlashlightController : MonoBehaviour
     void Update()
     {
         // Take a picture
-        if (Input.GetKeyDown(KeyCode.Space) && !isTakingPicture)
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && !isTakingPicture)
         {
             isTakingPicture = true;
             camSound.PlayRandom();
